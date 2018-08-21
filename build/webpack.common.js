8dc717780bbd4aebc99a0598dbd7d0e3062f957d
徐大大 <email@xluos.com>
2018-08-21 20:52:22 +0800

    开发模式加入热更新，babel移动到打包设置中，引入配置文件

--- build/webpack.common.js
+++ build/webpack.common.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const config = require('config-lite')(__dirname);
+
 module.exports = {
   entry: {
     app: './src/index.js'
@@ -13,13 +15,12 @@ module.exports = {
     }
     ),
     new HtmlWebpackPlugin({
-      title: 'Production',
       template: 'src/index.html',
-      templateParameters: true
+      title: config.title,
     })
   ],
   output: {
-    filename: '[name].bundle.[hash].js',
+    filename: '[name].js',
     path: path.resolve(__dirname, '../dist')
   },
   module: {
@@ -28,38 +29,17 @@ module.exports = {
     webpack 当遇到 import ... 时，会调用这里配置的 loader 对引用的文件进行编译
     */
     rules: [
-      {
-        /*
-        使用 babel 编译 ES6 / ES7 / ES8 为 ES5 代码
-        使用正则表达式匹配后缀名为 .js 的文件
-        */
-        test: /\.js$/,
 
-        // 排除 node_modules 目录下的文件，npm 安装的包不需要编译
-        exclude: /node_modules/,
-
-        /*
-        use 指定该文件的 loader, 值可以是字符串或者数组。
-        这里先使用 eslint-loader 处理，返回的结果交给 babel-loader 处理。loader 的处理顺序是从最后一个到第一个。
-        eslint-loader 用来检查代码，如果有错误，编译的时候会报错。
-        babel-loader 用来编译 js 文件。
-        */
-        use: [
-          'babel-loader',
-          // 'eslint-loader'
-        ]
-      },
-
-      {
-        // 匹配 html 文件
-        test: /\.html$/,
-        /*
-        使用 html-loader, 将 html 内容存为 js 字符串，比如当遇到
-        import htmlString from './template.html';
-        template.html 的文件内容会被转成一个 js 字符串，合并到 js 文件里。
-        */
-        use: 'html-loader'
-      },
+      // {
+      //   // 匹配 html 文件
+      //   test: /\.html$/,
+      //   /*
+      //   使用 html-loader, 将 html 内容存为 js 字符串，比如当遇到
+      //   import htmlString from './template.html';
+      //   template.html 的文件内容会被转成一个 js 字符串，合并到 js 文件里。
+      //   */
+      //   use: 'html-loader'
+      // },
 
       {
         // 匹配 css 文件
