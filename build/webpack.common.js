272fbb641cc0de8b9c339f3f4a5d1f17ec5f3f2e
xluos <email@xluos.com>
2018-10-28 18:15:28 +0800

    修复分离CSS后css文件引入的图片路径错误的问题

--- build/webpack.common.js
+++ build/webpack.common.js
@@ -99,7 +99,8 @@ module.exports = {
           {
             loader: 'url-loader',
             options: {
-              limit: 10000
+              limit: 10000,
+              name: 'images/[name].[ext]'
             }
           }
         ]
